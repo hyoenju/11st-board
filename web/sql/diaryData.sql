2d6a1b507a73a7a39b8f4cfb597a8b9b115c638b
mingsang <minsang0850@naver.com>
2022-02-25 01:48:06 +0900

    feat: 테스트용 diary 데이터 sql

---
+++ web/sql/diaryData.sql
@@ -0,0 +1,22 @@
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목11","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-01",0.73);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목11","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-02",0.43);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목12","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-03",0.23);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목13","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-04",0.13);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목14","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-05",0.53);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목15","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-06",0.63);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목16","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-07",0.73);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목17","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-08",0.83);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목18","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-09",0.63);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목19","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-10",0.23);
+INSERT INTO diary (title, content, hashtag, created_at,emotion_score)
+values("제목20","주말이라 오랜만에 푹 잘 수 있었다. 밀린 집안일도 끝내니 후련하다. 이제 하루종일 누워서 쉬어야겠다.","#행복","2022-02-11",0.93);
