c26d0128796403e77d5b6747add29cccd6c1d9fb
lhh5863 <lhh5863@dgu.ac.kr>
2022-02-24 12:00:54 +0900

    feat(api): #11 - Modify main.py
    - 계산된 스코어를 반환하는 코드 작성

--- api/main.py
+++ api/main.py
@@ -1,17 +1,15 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from function.sentiment_analysis import *
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-
 @app.get("/ml")
-def predict(q: Optional[str] = None):
-    pred = 0.99
-    return {"q": q, "pred": pred}
+def predict(content: Optional[str] = None):
+    score = predict_content_score(content)
+    return {"content": content, "score": score}
