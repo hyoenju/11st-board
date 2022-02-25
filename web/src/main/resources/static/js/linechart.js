1db791731463d4cedee1fe089fc53cf3a5fdedeb
Hyeonju Lee <l67378810@gmail.com>
2022-02-25 09:59:22 +0900

    feature/#31-emotion 감정 그래프 시각화 (#32)
    * feat(calendar) : #24 calendar 데이터를 index.html 에 출력
    
    * feat(diary) : #29 board_edit.html thymeleaf 수정
    
    * feat(diary) : #29 board_view.html thymeleaf 수정
    
    * feat(emotion) : #31 calendar 데이터 재구조화
    
    * feat(emotion) : #31 chart.js 로 데이터 전달

--- web/src/main/resources/static/js/linechart.js
+++ web/src/main/resources/static/js/linechart.js
@@ -20,8 +20,6 @@ const config = {
   }
 };
 
-var chart;
-
 function draw(data, labels) {
   chartData.labels = labels;
   chartData.datasets[0]['data'] = data;
@@ -30,16 +28,10 @@ function draw(data, labels) {
   .getElementById('chart')
   .getContext('2d');
 
-  chart = new Chart(context, config);
+  new Chart(context, config);
 }
 
 (function() {
-  const labels = ['1일', '2일', '3일', '4일', '5일', '6일', '7일', '8일', '9일', '10일', '11일', '12일', '13일', '14일', '15일',
-    '16일', '17일', '18일', '19일', '20일', '21일', '22일', '23일', '24일', '25일', '26일', '27일', '28일', '29일', '30일'
-  ];
-  // const data = [0.13, 0.41, , 0.64, 0, 0.5, null, 0.13, null, null, 0.47, 0.12, 0.19, 0.1, 0.1, 0.1, 0.1,
-  //   0.47, 0.1, 0.11, 0.25, 0.64, 0.29, 0.8, , 0.41, 0.07, , 0.5, 0.49];
-
   draw(scores, labels);
 })();
 
