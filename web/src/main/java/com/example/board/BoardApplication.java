653a9495e16eb8d717fd301050fb22b63bd72276
mingsang <minsang0850@naver.com>
2022-02-25 09:50:14 +0900

    feat(BoardApplication): Create Bean(HiddenHttpMethodFilter)
    Create bean for delete, put mapping

--- web/src/main/java/com/example/board/BoardApplication.java
+++ web/src/main/java/com/example/board/BoardApplication.java
@@ -3,6 +3,8 @@ package com.example.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
@@ -11,5 +13,9 @@ public class BoardApplication {
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
     }
-
+    
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
