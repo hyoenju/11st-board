75ac8349c7990ecd3d56c2b13c465790d85a1b61
minsang0850 <61973406+minsang0850@users.noreply.github.com>
2022-02-25 10:00:05 +0900

    python api를 사용해서 postMapping (#34)
    * docs: 검색 경로 설명 추가
    
    * fix(DiaryController): post,put,delete method return redirect address
    
    * fix: add emotion score to schema and domain
    
    * refactor(DiaryController): Modify view page return type to String
    
    * feat(DiaryController): add code using python server
    
    * fix: ModelAndView로 주소 맵핑
    
    * feat: 테스트용 diary 데이터 sql
    
    * feat(Calender): 달력 화면 api 구현
    
    * fix: divide exist to dateExist, diaryExist
    
    * feat: call python api and post api
    
    * feat(BoardApplication): Create Bean(HiddenHttpMethodFilter)
    
    
    Create bean for delete, put mapping

--- web/src/main/java/com/example/board/controller/DiaryController.java
+++ web/src/main/java/com/example/board/controller/DiaryController.java
@@ -2,18 +2,22 @@ package com.example.board.controller;
 
 import com.example.board.domain.Diary;
 import com.example.board.dto.CalenderDay;
+import com.example.board.dto.ScoreResponse;
 import com.example.board.service.DiaryService;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequiredArgsConstructor
@@ -22,7 +26,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class DiaryController {
 
     private final DiaryService diaryService;
-    //private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     @GetMapping
     public ModelAndView diaries() {
@@ -45,16 +49,17 @@ public class DiaryController {
         map.put("diary", diaryService.getDiary(diaryId));
         return new ModelAndView("diaries/board_view", map);
     }
-    //@PostMapping
-//    public String postDiary(Diary diary) {
-//        String baseUrl = "http://localhost:8000/ml?content=" + diary.getContent();
-//        ResponseEntity<ScoreResponse> responseEntity = restTemplate.getForEntity(baseUrl,
-//          ScoreResponse.class);
-//        double emotionScore = responseEntity.getBody().getScore();
-//        diary.setEmotionScore(emotionScore);
-//        Long diaryId = diaryService.postDiary(diary).getId();
-//        return "redirect:/diaries/"+diaryId;
-//    }
+    
+    @PostMapping
+    public String postDiary(Diary diary) {
+        String baseUrl = "http://localhost:8000/ml?content=" + diary.getContent();
+        ResponseEntity<ScoreResponse> responseEntity = restTemplate.getForEntity(baseUrl,
+          ScoreResponse.class);
+        double emotionScore = responseEntity.getBody().getScore();
+        diary.setEmotionScore(emotionScore);
+        Long diaryId = diaryService.postDiary(diary).getId();
+        return "redirect:/diaries/"+diaryId;
+    }
     
     @GetMapping("/write")
     public String writeDiary() {
